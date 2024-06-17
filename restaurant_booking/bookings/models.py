9b8b9b1b7db51e31afb04eee419432bf8065ba6b
Linus Apelgren <linusapelgren6789@gmail.com>
2024-06-17 16:22:46 +0000

    Added model for menu

--- restaurant_booking/bookings/models.py
+++ restaurant_booking/bookings/models.py
@@ -1,6 +1,6 @@
-# bookings/models.py
 from django.db import models
 from django.contrib.auth.models import User
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
@@ -22,6 +22,7 @@ class Table(models.Model):
 
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
@@ -29,3 +30,13 @@ class Reservation(models.Model):
 
     def __str__(self):
         return f"Reservation for {self.guests} guests at {self.table} on {self.date} at {self.time}"
+
+    
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
