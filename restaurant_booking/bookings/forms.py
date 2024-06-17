de0f5357dbca58e9f94fd05391f30b79aa5b843c
Linus Apelgren <linusapelgren6789@gmail.com>
2024-06-17 16:21:55 +0000

    Redisigned form for reservation

--- restaurant_booking/bookings/forms.py
+++ restaurant_booking/bookings/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Reservation
+from datetime import time
 
 class ReservationForm(forms.ModelForm):
+    name = forms.CharField(label='Your Name', max_length=100)
+    phone_number = forms.CharField(label='Phone Number', max_length=20)
+    guests = forms.IntegerField(label='Number of Guests', min_value=1, widget=forms.NumberInput(attrs={'type': 'number', 'min': '1', 'max': '10', 'step': '1'}))
+    date = forms.DateField(label='Date', widget=forms.SelectDateWidget())
+    time = forms.ChoiceField(label='Time', choices=[(time(hour, 0).strftime('%H:%M'), time(hour, 0).strftime('%I:%M %p')) for hour in range(9, 23)])
+
     class Meta:
         model = Reservation
-        fields = ['restaurant', 'date', 'time', 'guests']
+        fields = ['name', 'phone_number', 'guests', 'date', 'time']
